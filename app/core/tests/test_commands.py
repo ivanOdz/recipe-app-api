5dc6c35ffdf060459f2424bad2d7af3a664cad6a
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-06-05 11:12:17 -0300

    wait for db and flake

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,13 +10,15 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db when db is available"""
-        # Esto es como mockear y decir que el comando de si la bd esta lista, me devuelva true
+        # Esto es como mockear y decir que el comando de
+        # si la bd esta lista, me devuelva true
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -26,11 +28,10 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+                                    [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-
-
