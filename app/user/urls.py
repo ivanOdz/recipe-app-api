371663be4f433abd1d53ac082a389b9f6586c174
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-06-06 10:49:15 -0300

    User API completed

--- app/user/urls.py
+++ app/user/urls.py
@@ -10,4 +10,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserAPIView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserAPIView.as_view(), name='me'),
 ]
