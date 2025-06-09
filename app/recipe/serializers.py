8a623784b46897d2e2bbb71110a3dd92eb84e061
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-06-09 11:17:16 -0300

    Ingredient serializer, viewset, and get query

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,8 +6,17 @@ from rest_framework import serializers
 
 from core.models import (
     Recipe,
-    Tag
+    Tag,
+    Ingredient
 )
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for Ingredient model"""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id','name']
+        read_only_fields = ['id']
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag model"""
